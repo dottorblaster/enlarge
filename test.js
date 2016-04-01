71e018969d604cca67d9dfc2bc4eb05ccddace77
Gabriele <gabriele.stefanini@gmail.com>
2016-04-02 00:18:58 +0200

    what if...

--- test.js
+++ test.js
@@ -2,10 +2,14 @@ var test = require('tape'),
   enlarge = require('./');
 
 test('string padding', function(assert) {
-  assert.plan(5);
+  assert.plan(9);
   assert.strictEqual(enlarge('foobar', 9), '   foobar');
   assert.strictEqual(enlarge('foobar', 9, '@'), '@@@foobar');
   assert.strictEqual(enlarge('foobar', 9, 0), '000foobar');
   assert.strictEqual(enlarge('foobar', -4), 'foobar');
   assert.strictEqual(enlarge('foobar'), 'foobar');
+  assert.strictEqual(enlarge('foobar', 'a'), 'foobar');
+  assert.strictEqual(enlarge('foobar', 9, '+@'), '+++foobar');
+  assert.strictEqual(enlarge('foobar', 6, '@'), 'foobar');
+  assert.strictEqual(enlarge('foobar', 9, 10), '111foobar');
 });
